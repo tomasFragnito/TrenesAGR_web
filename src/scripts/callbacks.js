120fe6058271e169a54e479c3186b4eb766711ef
tomasfrg <tomignacio2022@gmail.com>
2026-03-05 19:13:32 -0300

    modificacion del puerto

--- src/scripts/callbacks.js
+++ src/scripts/callbacks.js
@@ -1,6 +1,6 @@
 
 //const BASE_URL = "http://localhost:8080/api";
-const BASE_URL = "https://trenesagr.onrender.com:8080/api";
+const BASE_URL = "https://trenesagr.onrender.com/api";
 
 export const getProximosArribos = async (id) => {
     try {
